656ab46f8967a7ad9bb3c38bf8f5e968612d7fac
= <=>
2026-09-21 12:25:01 +0200

    Unity midi lyd til spike

--- test-files/color_test.py
+++ test-files/color_test.py
@@ -45,8 +45,8 @@ async def forhin1(obs):
     print("Done crossing, total bars:", count)
     await grå_linje(obs)
 
-# pt. 2
-async def forhin1(obs):
+# Opgave 2 (brudt streg)
+async def forhin2(obs):
     motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 45, 0 ,100)
     await runloop.sleep_tank_ms(500)
 
@@ -68,6 +68,32 @@ async def forhin1(obs):
     motor_pair.stop(motor_pair.PAIR_1)
     print("Done crossing, total bars:", count)
     await grå_linje(obs)
+
+#Opgave 3 Skub flaske 
+
+async def forhin3(obs):
+    motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 45, 0 ,100)
+    await runloop.sleep_tank_ms(500)
+
+    motor_pair.move_tank(motor_pair.PAIR_1, 250, 250)
+    count = 0
+    on_bar = False
+
+    while count < 1:
+        reflection = color_sensor.reflection(port.C)
+        currently_on_bar = reflection < THRESHOLD
+
+        if currently_on_bar and not on_bar:
+            count += 1
+            print("Bars counted:", count)
+
+        on_bar = currently_on_bar
+        await runloop.sleep_ms(10)
+
+    motor_pair.stop(motor_pair.PAIR_1)
+    print("Done crossing, total bars:", count)
+    await grå_linje(obs)
+
 
 
 
