22895e5001cbf0b2a46a581ed63dc633aaf4bb56
Fewwick <30862308+Fewwick@users.noreply.github.com>
2026-09-22 10:03:13 +0200

    Rename Brudt_streg1&2.py to Opg1&2-Brudt_streg.py

--- Polished/Individual_challenges/Opg1&2-Brudt_streg.py
+++ Polished/Individual_challenges/Opg1&2-Brudt_streg.py
@@ -5,6 +5,21 @@ import motor_pair
 import color_sensor
 import color
 
+obs = 0
+THRESHOLD = 75# reflection value below this = "on a bar" — tune on your mat
+motor_pair.pair(motor_pair.PAIR_1, port.B, port.A)
+
+async def grå_linje(obs):
+    while True:
+        if color_sensor.color(port.C) == color.BLACK or color_sensor.color(port.D) == color.BLACK:
+            motor_pair.stop(motor_pair.PAIR_1)
+            obs = obs + 1
+            await forhin5(obs)
+            continue
+        else:
+            motor_pair.move_tank(motor_pair.PAIR_1, color_sensor.reflection(port.D)*2, color_sensor.reflection(port.C)*2)
+        await runloop.sleep_ms(10)
+
 # Opgave 1 (brudt strej)
 async def forhin1(obs):
     motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 100, 0 ,45)
@@ -53,3 +68,16 @@ async def forhin2(obs):
     motor_pair.stop(motor_pair.PAIR_1)
     print("Done crossing, total bars:", count)
     await grå_linje(obs)
+
+
+
+async def main(x):
+    if x == 1:
+        print(1)
+    elif x == 2:
+        print(2)
+    elif x==5:
+        await forhin5(x)
+
+
+runloop.run(grå_linje(obs))
